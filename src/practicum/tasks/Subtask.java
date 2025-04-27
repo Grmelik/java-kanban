88bfcc27d2fe9f9e6a5405f8a7dddb9b610753fe
Byswork <byswork@yandex.ru>
2025-04-27 23:53:19 +0300

    Исправлены ошибки в codestyle

--- src/practicum/tasks/Subtask.java
+++ src/practicum/tasks/Subtask.java
@@ -1,6 +1,6 @@
 package practicum.tasks;
 
-public class Subtask extends Task{
+public class Subtask extends Task {
     private int epicId; // Id родительского эпика
 
     public Subtask(String name, String description, TaskStatus status) {
